b7aa5c29464fa5a8da61cdcb4ec7d89051afb173
Pavel Ivanenko <pivanenko@dataxu.com>
2014-11-24 20:07:37 +0200

    spring 4

---
+++ src/main/java/org/oracul/service/controller/HelloController.java
@@ -0,0 +1,15 @@
+package org.oracul.service.controller;
+
+import org.oracul.service.dto.Greetings;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public Greetings printWelcome() {
+		return new Greetings("hi");
+	}
+}
---
+++ src/main/java/org/oracul/service/dto/Greetings.java
@@ -0,0 +1,13 @@
+package org.oracul.service.dto;
+
+public class Greetings {
+	private String message;
+
+	public Greetings(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
