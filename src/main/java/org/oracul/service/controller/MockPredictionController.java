90c7dc231ebe372ac6a1de46d66afa378782f79e
Pavel Ivanenko <pivanenko@dataxu.com>
2014-11-26 17:24:36 +0200

    oracul is executed from service

--- src/main/java/org/oracul/service/controller/MockPredictionController.java
+++ src/main/java/org/oracul/service/controller/MockPredictionController.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 
 @RestController
 public class MockPredictionController {
+
 	@Autowired
 	private PredictionBuilder builder;
 
--- src/main/java/org/oracul/service/controller/SingletonPrognosisController.java
+++ src/main/java/org/oracul/service/controller/SingletonPrognosisController.java
@@ -1,14 +1,53 @@
 package org.oracul.service.controller;
 
+import org.apache.log4j.Logger;
+import org.oracul.service.builder.PredictionBuilder;
 import org.oracul.service.dto.Prediction2D;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+
 @RestController
 public class SingletonPrognosisController {
+
+	private static final Logger logger = Logger.getLogger(SingletonPrognosisController.class);
+
+	@Autowired
+	private PredictionBuilder builder;
+
+	@Autowired
+	private ResourceLoader resourceLoader;
+
+	@Value("${oracul.execute.dir}")
+	private String executeOraculDir;
+	@Value("${oracul.execute.command}")
+	private String executeOraculCommand;
+
 	@RequestMapping(value = "/singleton", method = RequestMethod.GET)
-	public Prediction2D getMockPrediction() {
-		return null;
+	public synchronized Prediction2D getMockPrediction() {
+		try {
+			ProcessBuilder builder = new ProcessBuilder(executeOraculCommand);
+			builder.directory(new File(executeOraculDir));
+			Process start = builder.start();
+			start.waitFor();
+
+//			ProcessBuilder builder = new ProcessBuilder("./sleep.sh");
+//			builder.directory(new File("/Users/metzgermeister/temp/"));
+//			Process start = builder.start();
+//			start.waitFor();
+
+			if (logger.isDebugEnabled()) {
+				logger.debug("executed oracul");
+			}
+		} catch (Exception e) {
+			logger.error("failed to execute oracul", e);
+			throw new RuntimeException(e);
+		}
+		return new Prediction2D(new double[]{42}, new double[]{42}, 1, 1);
 	}
 }
